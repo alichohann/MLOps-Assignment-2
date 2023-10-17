e95117456ac5188563ba410b25a96c5eac165104
alichohan <achohanali969@gmail.com>
2023-10-17 05:30:55 +0500

    task.py update

--- task.py
+++ task.py
@@ -1,42 +1,44 @@
 import pandas as pd
+import numpy as np
 from sklearn import datasets
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-# Load the Boston Housing Dataset
-boston_housing = datasets.load_boston()
-data = pd.DataFrame(data=boston_housing.data,
-                    columns=boston_housing.feature_names)
-target = pd.Series(boston_housing.target, name='target')
+# Load the Iris dataset
+iris = datasets.load_iris()
+data = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+target = pd.Series(iris.target, name='target')
 
 # Select the feature and target variable
-X = data.drop(['MEDV'], axis=1).values
+X = data['sepal length (cm)'].values.reshape(-1, 1)
 y = target
 
 # Split the data into training and testing sets
-Xtr, Xts, ytr, yts = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a random forest model
-model = RandomForestRegressor()
+# Create a linear regression model
+model = LinearRegression()
 
 # Train the model on the training data
-model.fit(Xtr, ytr)
+model.fit(X_train, y_train)
 
 # Make predictions on the test data
-y_pred = model.predict(Xts)
+y_pred = model.predict(X_test)
 
 # Evaluate the model
-mse = mean_squared_error(yts, y_pred)
-r2 = r2_score(yts, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-print(f'Mean Squared Error: {mse:.2f}')
-print(f'R-squared (Coefficient of Determination): {r2:.2f}')
+# print(f'Mean Squared Error: {mse:.2f}')
+#print(f'R-squared (Coefficient of Determination): {r2:.2f}')
 
-plt.scatter(yts, y_pred, color='black')
-plt.plot([0, 50], [0, 50], color='blue', linewidth=3)
-plt.xlabel('Actual Price')
-plt.ylabel('Predicted Price')
-plt.title('Random Forest Regression on Boston Housing Dataset')
+# Visualize the regression line (optional)
+import matplotlib.pyplot as plt
+
+plt.scatter(X_test, y_test, color='black')
+plt.plot(X_test, y_pred, color='blue', linewidth=3)
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Target')
+plt.title('Linear Regression on Sepal Length')
 plt.show()
